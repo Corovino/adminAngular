2575eeb570bee6050775e199cccf30729c2a7ee3
Hebert Romero <hjr@iqthink.com>
2017-08-23 09:31:03 -0500

    conflictos de subida ajustes aside-menu - data binding newTest

--- src/app/admin/test/newtest/newtest.component.ts
+++ src/app/admin/test/newtest/newtest.component.ts
@@ -17,9 +17,55 @@ export class NewtestComponent implements OnInit {
     {value: 'option2', viewValue: 'Option 2'},
     {value: 'option3', viewValue: 'Option 3'}
   ];
+
+
+  public status : any;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.status = {
+       problem : true,
+       solution : false,
+       rules : false,
+       position : false
+    }
+
+    console.log(this.status.problem);
+  }
+
+
+  asideStatus( status : string)
+  {
+
+       switch (status){
+         case "problem":
+           this.status.problem = true;
+           this.status.solution = false;
+           this.status.rules = false;
+           this.status.position = false;
+           break;
+         case "solution":
+           this.status.problem = false;
+           this.status.solution = true;
+           this.status.rules = false;
+           this.status.position = false;
+           break;
+         case "rules":
+           this.status.problem = false;
+           this.status.solution = false;
+           this.status.rules = true;
+           this.status.position = false;
+           break;
+         case "position":
+           this.status.problem = false;
+           this.status.solution = false;
+           this.status.rules = false;
+           this.status.position = true;
+           break;
+       }
+
   }
 
 }
